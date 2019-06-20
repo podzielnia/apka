65a7b8c2aba43c4435b43b9dd61b5ddaa9941ecc
Jakub Banaś <kuba792@gmail.com>
2019-06-20 14:17:49 +0200

    add simple answer picker

--- src/components/AnswerPicker.tsx
+++ src/components/AnswerPicker.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
 
-const AnswerPicker: React.FC = () => {
-  return <div />;
-};
-
-export default AnswerPicker;
+export default function AnswerPicker() {
+  return (
+    <Grid item xs={12}>
+      <ButtonGroup fullWidth variant="contained">
+        <Button>Wyrzucam do kosza</Button>
+        <Button>Oddaje w punkcie zbiórki elektroodpadów</Button>
+      </ButtonGroup>
+    </Grid>
+  );
+}
