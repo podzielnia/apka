69c7a7e4ae58548f5b151779ba6c2b082d50aa89
Jakub Banaś <kuba792@gmail.com>
2019-06-20 08:15:48 +0200

    question page logic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import QuestionText from "./components/QuestionText";
+import QuestionImage from "./components/QuestionImage";
+import AnswerPicker from "./components/AnswerPicker";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <QuestionText />
+      <QuestionImage />
+      <AnswerPicker />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/QuestionText.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const QuestionText: React.FC = () => {
+  return <div />;
+};
+
+export default QuestionText;
