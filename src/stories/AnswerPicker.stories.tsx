bf4cfed5e28022e17f5a8c873a60ee15050789ef
Jakub Banas <kuba792@gmail.com>
2019-07-10 22:25:32 +0200

    remove temp questions list

--- src/stories/AnswerPicker.stories.tsx
+++ src/stories/AnswerPicker.stories.tsx
@@ -5,7 +5,19 @@ import { object, withKnobs } from '@storybook/addon-knobs/react';
 import { storiesOf } from '@storybook/react';
 
 import AnswerPicker from '../components/AnswerPicker';
-import { questions } from '../questions';
+
+export const question = {
+  id: 0,
+  title: 'Planujesz wakacje..',
+  answers: [
+    { text: 'Pociąg i rower, w przyrodzie', isCorrect: true },
+    { text: 'Byle jak najdalej, samolotem', isCorrect: false },
+  ],
+  hints: [
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nibh augue, suscipit a, scelerisque sed, lacinia in, mi. Cras vel lorem. Etiam pellentesque aliquet tellus. ',
+    'Phasellus pharetra nulla ac diam. Quisque semper justo at risus. Donec venenatis, turpis vel hendrerit interdum, dui ligula ultricies purus, sed posuere libero dui id orci. Nam congue, pede vitae dapibus aliquet,',
+  ],
+};
 
 export const actions = {
   onPick: action('onPick'),
@@ -14,8 +26,5 @@ export const actions = {
 storiesOf('Question', module)
   .addDecorator(withKnobs)
   .add('AnswerPicker', () => (
-    <AnswerPicker
-      {...actions}
-      answers={object('answers', questions[0].answers)}
-    />
+    <AnswerPicker {...actions} answers={object('answers', question.answers)} />
   ));
--- src/stories/Question.stories.tsx
+++ src/stories/Question.stories.tsx
@@ -5,7 +5,19 @@ import { storiesOf } from '@storybook/react';
 
 import { action } from '@storybook/addon-actions';
 import QuestionView from '../components/QuestionView';
-import { questions } from '../questions';
+
+export const question = {
+  id: 0,
+  title: 'Planujesz wakacje..',
+  answers: [
+    { text: 'Pociąg i rower, w przyrodzie', isCorrect: true },
+    { text: 'Byle jak najdalej, samolotem', isCorrect: false },
+  ],
+  hints: [
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nibh augue, suscipit a, scelerisque sed, lacinia in, mi. Cras vel lorem. Etiam pellentesque aliquet tellus. ',
+    'Phasellus pharetra nulla ac diam. Quisque semper justo at risus. Donec venenatis, turpis vel hendrerit interdum, dui ligula ultricies purus, sed posuere libero dui id orci. Nam congue, pede vitae dapibus aliquet,',
+  ],
+};
 
 export const actions = {
   onPick: action('onPick'),
@@ -14,5 +26,5 @@ export const actions = {
 storiesOf('Question', module)
   .addDecorator(withKnobs)
   .add('QuestionView', () => (
-    <QuestionView question={object('question', questions[0])} {...actions} />
+    <QuestionView question={object('question', question)} {...actions} />
   ));
