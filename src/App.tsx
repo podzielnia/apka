81e7b3a481e71d6b7a73ae06c2677d71a8055bcb
Jakub Banas <kuba792@gmail.com>
2019-07-10 21:09:38 +0200

    add fake store

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import Main from './components/Main';
+import rootReducer from './store/reducers/rootReducer';
+
+const store = createStore(rootReducer);
 
 const App: React.FC = () => {
-  return <Main />;
+  return (
+    <Provider store={store}>
+      <Main />
+    </Provider>
+  );
 };
 
 export default App;
