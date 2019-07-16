448f3c58d43ebf567e185663f66df861610ced2c
Jakub Banas <kuba792@gmail.com>
2019-07-16 18:11:03 +0200

    remove question reducer

--- src/store/reducers/rootReducer.ts
+++ src/store/reducers/rootReducer.ts
@@ -2,15 +2,18 @@ import { firebaseReducer } from 'react-redux-firebase';
 import { combineReducers } from 'redux';
 import { firestoreReducer } from 'redux-firestore';
 
-import questionsReducer, { QuestionReducerState } from './questionReducer';
+import { Question } from 'types/Question';
+
+export interface QuestionsState {
+  questions: Question[];
+}
 
 export interface ReduxState {
-  question: QuestionReducerState;
+  question: QuestionsState;
   firestore: any;
 }
 
 const rootReducer = combineReducers({
-  question: questionsReducer,
   firebase: firebaseReducer,
   firestore: firestoreReducer,
 });
