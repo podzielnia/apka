bd4288c3550b4732415676e5d6b1a1f3f02a5dea
Monika <molinskamonika+github@gmail.com>
2019-08-26 16:35:08 +0200

    move material ThemeProvider to App

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,9 @@
-import { MuiThemeProvider } from '@material-ui/core/styles';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import CustomTheme from './CustomTheme';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <MuiThemeProvider theme={CustomTheme}>
-    <App />
-  </MuiThemeProvider>,
-  document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.register();
